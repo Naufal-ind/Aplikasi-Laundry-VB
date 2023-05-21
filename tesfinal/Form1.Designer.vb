df516d101d2618ea0d11aa31368af0bf60a1bd85
Naufal-ind <naufalindiarto17@gmail.com>
2023-05-21 11:28:38 +0700

    Upload Apk VIsual Basic Pertama

---
+++ tesfinal/Form1.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtBerat = New System.Windows.Forms.TextBox()
+        Me.txtNoOrder = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.cbServis = New System.Windows.Forms.ComboBox()
+        Me.cbJenisCuci = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(358, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "APLIKASI LAUNDRY"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(391, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "KUCEX"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(51, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "NO ORDER"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(51, 131)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(115, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "NAMA PELANGGAN"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(51, 175)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "BERAT"
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(172, 128)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(145, 23)
+        Me.txtNama.TabIndex = 5
+        '
+        'txtBerat
+        '
+        Me.txtBerat.Location = New System.Drawing.Point(172, 172)
+        Me.txtBerat.Name = "txtBerat"
+        Me.txtBerat.Size = New System.Drawing.Size(145, 23)
+        Me.txtBerat.TabIndex = 6
+        '
+        'txtNoOrder
+        '
+        Me.txtNoOrder.Location = New System.Drawing.Point(172, 88)
+        Me.txtNoOrder.Name = "txtNoOrder"
+        Me.txtNoOrder.Size = New System.Drawing.Size(145, 23)
+        Me.txtNoOrder.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(333, 175)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(21, 15)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "KG"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(410, 91)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(65, 15)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "JENIS CUCI"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(410, 131)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(91, 15)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "JENIS LAYANAN"
+        '
+        'cbServis
+        '
+        Me.cbServis.FormattingEnabled = True
+        Me.cbServis.Items.AddRange(New Object() {"BIASA", "KILAT"})
+        Me.cbServis.Location = New System.Drawing.Point(522, 128)
+        Me.cbServis.Name = "cbServis"
+        Me.cbServis.Size = New System.Drawing.Size(123, 23)
+        Me.cbServis.TabIndex = 11
+        '
+        'cbJenisCuci
+        '
+        Me.cbJenisCuci.FormattingEnabled = True
+        Me.cbJenisCuci.Items.AddRange(New Object() {"CUCI KERING", "CUCI+SETRIKA"})
+        Me.cbJenisCuci.Location = New System.Drawing.Point(522, 88)
+        Me.cbJenisCuci.Name = "cbJenisCuci"
+        Me.cbJenisCuci.Size = New System.Drawing.Size(123, 23)
+        Me.cbJenisCuci.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(178, 217)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "HITUNG"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(410, 217)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "BATAL"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.DataGridView1.Location = New System.Drawing.Point(81, 269)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(641, 150)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "NO ORDER"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "NAMA PELANGGAN"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "BERAT"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "JENIS CUCI"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "JENIS LAYANAN"
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "TOTAL HARGA"
+        Me.Column6.Name = "Column6"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cbJenisCuci)
+        Me.Controls.Add(Me.cbServis)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtNoOrder)
+        Me.Controls.Add(Me.txtBerat)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtBerat As TextBox
+    Friend WithEvents txtNoOrder As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents cbServis As ComboBox
+    Friend WithEvents cbJenisCuci As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+End Class
---
+++ tesfinal/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Dim harga As Double
+    Dim TotalHarga As Double
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtNoOrder.Text = DateAndTime.Now.ToString("yyMMdd-hhmmss")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If cbJenisCuci.Text = "CUCI KERING" And cbServis.Text = "BIASA" Then
+            harga = 3000
+        ElseIf cbJenisCuci.Text = "CUCI KERING" And cbServis.Text = "KILAT" Then
+            harga = 5000
+        ElseIf cbJenisCuci.Text = "CUCI+SETRIKA" And cbServis.Text = "BIASA" Then
+            harga = 6000
+        ElseIf cbJenisCuci.Text = "CUCI+SETRIKA" And cbServis.Text = "KILAT" Then
+            harga = 7000
+        End If
+
+        If txtNama.Text = "" Or txtBerat.Text = "" Or cbJenisCuci.Text = "" Or cbServis.Text = "" Then
+            MsgBox("ISI FORMULIR DULULAH!")
+        Else
+            TotalHarga = txtBerat.Text * harga
+            MessageBox.Show("NO ORDER : " + txtNoOrder.Text & Environment.NewLine &
+                            "NAMA PELANGGAN : " + txtNama.Text & Environment.NewLine &
+                            "JUMLAH BERAT : " + txtBerat.Text & Environment.NewLine &
+                            "JENIS & LAYANAN : " + cbJenisCuci.Text + " " + cbServis.Text & Environment.NewLine &
+                            "TOTAL HARGA : " + TotalHarga.ToString)
+        End If
+
+        DataGridView1.Rows.Add(1)
+        DataGridView1.Rows(DataGridView1.RowCount - 2).Cells(0).Value = txtNoOrder.Text
+        DataGridView1.Rows(DataGridView1.RowCount - 2).Cells(1).Value = txtNama.Text
+        DataGridView1.Rows(DataGridView1.RowCount - 2).Cells(2).Value = txtBerat.Text
+        DataGridView1.Rows(DataGridView1.RowCount - 2).Cells(3).Value = cbJenisCuci.Text
+        DataGridView1.Rows(DataGridView1.RowCount - 2).Cells(4).Value = cbServis.Text
+        DataGridView1.Rows(DataGridView1.RowCount - 2).Cells(5).Value = TotalHarga
+        DataGridView1.Update()
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtNoOrder.Text = DateAndTime.Now.ToString("yyMMdd-hhmmss")
+        txtNama.Text = ""
+        txtBerat.Text = ""
+        cbJenisCuci.Text = ""
+        cbServis.Text = ""
+    End Sub
+End Class
